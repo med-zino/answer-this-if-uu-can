0b23744b0d1418460cab6e6abf09bc9daf65ced7
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2021-02-19 16:27:54 +0100

    :sparkles: displaying messages according to answers

--- training.js
+++ training.js
@@ -13,12 +13,13 @@ question.prototype.typeQuestion = function () {
     return suggestions;
 }
 
-var answers = 
-  
+var answers =
+
     ' It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
 
 question.prototype.checkAnswer = function (response) {
-    document.write(response == this.correct ? answers : 'please reload the page and start again');
+    //document.write(response == this.correct ? answers : 'please reload the page and start again');
+    document.getElementById(response == this.correct ? "correct" : "wrong").style.display = "block";
 }
 
 question.prototype.toText = function () {
@@ -33,7 +34,7 @@ var q4 = new question('how many departements do we have', ['4', '3'], 1)
 
 
 //creating a quiz
-var Quz = [q1, q2, q3 , q4];
+var Quz = [q1, q2, q3, q4];
 
 //genrating a random question
 n = Math.floor(Math.random() * 4);
